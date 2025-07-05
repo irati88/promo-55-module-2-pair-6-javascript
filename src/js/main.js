f6dfee228ea6383a6fbd5c5913c1510ecc746cbb
irati88 <irati.a88@gmail.com>
2025-07-05 16:28:39 +0200

    segunda parte

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-console.log('>> Ready :)');
+// 1. Mensaje de navegador obsoleto
+
+document.querySelector('h1').innerHTML
+console.log('Estás mostando el h1');
+
+
+
+// 2. Hola Mundo
+
+
+
+// 3. Seleccionando Adalabers
+
+
+
+// 4. Lorem ipsum
+
+
+// 5. Arreglando errores
+
+
+// Segunda parte//
+
+//1.-Cómo te llamas?//
+
+const inputNombre = document.querySelector('.nombreInput');
+const botonSaludar = document.querySelector('.helloBtn');
+const spanMessage = document.querySelector('.message');
+botonSaludar.addEventListener('click', () => {
+    const nombre = inputNombre.value;
+    spanMessage.innerHTML = 'Hola ' + nombre;
+    console.log('Estás escribiendo en el input');
+});
+
+
+//2.- Cambia el estilo de un botón//
+
+const myButton = document.querySelector('.button');
+myButton.addEventListener("click", (event) => {
+    myButton.classList.toggle('active');
+    console.log("Has hecho click");
+})
