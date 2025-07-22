f983e593a64adfc99dd81a0475779171c81bea66
Karengonsan <karenglezschez@gmail.com>
2025-07-22 18:06:17 +0200

    actualizacion ejercicio 2.8.3

--- modulo-2-leccion-8-Bucles/2.8.3 Tenemos mucho en común/main.js
+++ modulo-2-leccion-8-Bucles/2.8.3 Tenemos mucho en común/main.js
@@ -1,3 +1,21 @@
 "use strict";
 
 console.log("Tamoh ready");
+
+const inputOne = document.querySelector('.input--one');
+const inputTwo = document.querySelector('.input--two');
+const buttonBook = document.querySelector('.button--book');
+const paragraphBook = document.querySelector('.paragraph--book');
+
+const books = [];
+
+const handleClick = () => {
+    console.log('Botón pulsado');
+    const resultOne = inputOne.value;
+    const resultTwo = inputTwo.value;
+    books.push(resultOne, resultTwo);
+    for (const book of books) {
+        paragraphBook.innerHTML = ('¡A mí tambien me encantó' + ' ' + book +  ' ' + 'tenemos mucho en común, humana!');
+    }
+};
+buttonBook.addEventListener('click', handleClick);
