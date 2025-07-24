9176e655a6e23e42b2b0ea4d002d6990dfec262d
Karengonsan <karenglezschez@gmail.com>
2025-07-24 19:58:28 +0200

    actualizacion ejercicio9real

--- modulo-2-leccion-9 Arrays y bucles/2.9.1 Crear un listado de números/main.js
+++ modulo-2-leccion-9 Arrays y bucles/2.9.1 Crear un listado de números/main.js
@@ -11,3 +11,38 @@ const get100Numbers = () => {
 };
 const arrayResult = get100Numbers (); //Aqui guardo el retorno de la funcion y la invoco.
 console.log(arrayResult);
+
+//1.2
+
+const getReversed100Numbers = () => {
+  arrayResult.reverse; // llama y obten el array [1-100]
+  return arrayResult.reverse(); // invierte y devuelve
+};
+
+const arrayInvertido = getReversed100Numbers();
+console.log(arrayInvertido);
+
+console.log("----------------------------------");
+
+// Ejercicio 2: recorrer el array con un bucle si es par lo añadimos al de pares y si no al multiplo de 3, usamos el if else, cuando acabe el buble le ponemos un concat
+
+const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+const bestLostNumber = () => {
+  const Pares = [];
+  const MultiplosDeTres = [];
+  lostNumbers.forEach((Number) => {
+    if (Number % 2 === 0) {
+      Pares.push(Number);
+    }
+    if (Number % 3 === 0) {
+      MultiplosDeTres.push(Number);
+    }
+  });
+  console.log(Pares);
+  console.log(MultiplosDeTres);
+  const concateNumbers = Pares.concat(MultiplosDeTres);
+  return concateNumbers;  
+};
+const resulConcat = bestLostNumber();
+console.log(resulConcat);
